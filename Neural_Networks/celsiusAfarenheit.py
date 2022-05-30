f55b251cf41abfa61dafbc4dbdaf4034e5461c10
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2022-05-29 21:48:42 -0500

    created gitignore

--- Neural_Networks/celsiusAfarenheit.py
+++ Neural_Networks/celsiusAfarenheit.py
@@ -1,6 +1,22 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 #Datos de entrenamiento
 celsius = np.array([-40,-10,0,8,15,22,38],dtype=float)
 fahrenheit = np.array([-40,14,32,46,59,72,100], dtype=float)
+
+#Se tiene una capa densa con 1 neurona y que está conectada a una capa de entrada 
+capa = tf.keras.layers.Dense(units=1, input_shape=[1]) 
+modelo = tf.keras.Sequential([capa])
+
+modelo.compile(
+    optimizer=tf.keras.optimizers.Adam(0.1),
+    loss='mean_squared_error'
+)
+historial = modelo.fit(celsius,fahrenheit, epochs=1000, verbose=True)
+print("Entrenamiento terminado")
+
+plt.xlabel("Serie")
+plt.ylabel("Perdida")
+plt.plot(historial.history["loss"])
